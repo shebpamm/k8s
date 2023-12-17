ac81e402559ed5eba9f8344132bd7f7d5c3e4552
Erik Karsten <erik@opcode.xyz>
2023-12-17 17:36:16 +0200

    modify theme

--- devenv.nix
+++ devenv.nix
@@ -3,4 +3,6 @@
 {
   packages = with pkgs; [ hubble terraform cilium-cli talosctl ];
   env.VAULT_ADDR = "https://vault.sorsa.cloud";
+  env.theme_display_k8s_context = "yes";
+  env.theme_color_scheme = "dracula";
 }
